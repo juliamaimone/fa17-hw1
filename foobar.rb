2f1a9104c45a3f13a6e6b5890c79e6c33dde72b9
Julia <julia.maimone@berkeley.edu>
2017-09-17 14:04:28 -0700

    hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,11 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    sum = 0
+    a = a.map{ |elm| elm.to_i }
+    a = a.uniq.map{ |e| e += 2}.select{|x| x < 10}.select{ |y| y % 2 == 0}.each{|z| sum += z}
+    return sum
+
   end
 end
 
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,19 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
 
+  # Q1 CODE HERE
+  return (a + b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  return a.sort.collect {|n| n + 1}
+ 
 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  return first_name + " " + last_name
 
 end
 
